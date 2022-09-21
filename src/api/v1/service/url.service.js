0347a75dc24f38abdaf498a2d053d429b897b265
nikitdwivedi <nikitdwivedi1997@gmail.com>
2022-09-21 18:12:33 +0530

    username

--- src/api/v1/service/url.service.js
+++ src/api/v1/service/url.service.js
@@ -1,4 +1,4 @@
-let baseUrl = "http://line.premium-dino.com/player_api.php?username=aaccdfdef8&password=669f067695"
+let baseUrl = "http://line.premium-dino.com/player_api.php?username=ff5d039692&password=1384c85f6b"
 
 module.exports = {
     categoryUrl: () => {
