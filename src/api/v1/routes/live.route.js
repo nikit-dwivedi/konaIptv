74031b83b673b468a7bc27691b6ee412d678ae36
nikitdwivedi <nikitdwivedi1997@gmail.com>
2022-09-21 18:14:13 +0530

    token

--- src/api/v1/routes/live.route.js
+++ src/api/v1/routes/live.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getCategory, getliveByCategory } = require('../controller/live.controller.js');
 const { authenticateVerifiedyUser } = require("../middleware/authToken.js");
 
-router.get('/category', getCategory);
+router.get('/category',authenticateVerifiedyUser, getCategory);
 router.get('/category/:categoryId', authenticateVerifiedyUser, getliveByCategory);
 
 module.exports = router;
