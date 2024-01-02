3f0e8f7ad20bfe698d24c7f5da485a30432d608c
Nikit_dwivedi <nikitdwivedi1997@gmail.com>
2024-01-02 16:16:09 +0530

    change

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var fs = require('fs');
 const http = require('http');
-const https = require('https');
 const app = require('./app');
 require('dotenv').config()
 process.env.TZ = 'Asia/Calcutta';
@@ -8,16 +6,10 @@ const port = process.env.PORT || 4001;
 const IP = process.env.IP || '127.0.0.1';
 
 
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-let credentials = {key: privateKey, cert: certificate};
+
 
 const server = http.createServer(app);
-const secureServer = https.createServer(credentials,app);
 
-secureServer.listen(8008, () => {
-    console.log(`TevisTv listening at http://${IP}:8008`);
-})
 server.listen(port, () => {
     console.log(`TevisTv listening at http://${IP}:${port}`)
 });
